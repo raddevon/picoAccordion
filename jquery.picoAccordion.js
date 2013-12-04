5f11fea8a2123c203376b634fb7f6f6d0c360ad6
Devon Campbell <raddevon@gmail.com>
2013-12-04 10:15:29 -0500

    Adds readme file

---
+++ jquery.picoAccordion.js
@@ -0,0 +1,83 @@
+/*!
+ * picoAccordion
+ * Author: Devon Campbell (@raddevon)
+ * Licensed under the MIT license
+ */
+
+;(function ( $, window, document, undefined ) {
+
+    var pluginName = "picoAccordion",
+        defaults = {
+            duration: "0.2s",
+            // Determines whether accordion elements begin opened or closed
+            open: false,
+            // The maximum width at which the accordion will work. Set this
+            // if you only want the accordion functionality on mobile.
+            maxWidth: null
+        };
+
+    function Plugin( element, options ) {
+        this.element = element;
+
+        this.options = $.extend( {}, defaults, options) ;
+
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    Plugin.prototype = {
+
+        init: function() {
+            $el = $(this.element);
+            thisObject = this;
+
+            $el.children('dt')
+                // Binds the click on dt elements to toggle the adjacent element
+                .on('click', function(e){
+                    thisObject.widthTest(thisObject.accordionToggle, $(this).next(), thisObject.options);
+                })
+                // Sets the hand pointer on each of the dt elements
+                .each(function() {
+                    $(this).css('cursor', 'pointer')
+                });
+            thisObject.widthTest(thisObject.accordionSet, $('dl').find('dd'), thisObject.options);
+        },
+
+        // Checks if the window is less than options.maxWidth wide before running the passed function
+        widthTest: function(func, $el, options) {
+            if (options.maxWidth) {
+                if ($(window).width() <= options.maxWidth) {
+                    func($el, options);
+                }
+            } else {
+                func($el, options);
+            }
+        },
+
+        // Toggles the accordion element
+        accordionToggle: function($el, options) {
+            $el.slideToggle();
+        },
+
+        // Sets the default accordion element states based on the open option
+        accordionSet: function($el, options) {
+            if (options.open) {
+                $el.show();
+            } else {
+                $el.hide();
+            }
+        }
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,
+                new Plugin( this, options ));
+            }
+        });
+    };
+
+})( jQuery, window, document );
---
+++ readme.md
@@ -0,0 +1,36 @@
+# picoAccordion
+
+Have you ever wished there was an accordion plugin for jQuery that you could just plug in to any of your projects? Wait, what? You say there are tons of them? Are you **sure** about that? Go look to make sure. I'll wait for you.
+
+
+
+
+
+Oh, you say there were literally thousands of them? Hmm. OK. Well, did you find one that was absolutely perfect in every way? I thought not. Well, maybe picoAccordion is that plugin. It does have some distinguishing features.
+
+* *Small*- picoAccordion is 826 bytes minified. At that size, you can afford to pack a few more of those 12 megapixel JPEGs onto your page!
+* *Opinionated*- picoAccordion believes strongly that the definition list is the most semantic element for an accordion -- so strongly, in fact, that it won't even work if you try to use it with some lesser element. You can call it laziness, but it prefers to think of it as conscientious objection.
+* *Umm*- I wanted to include another bullet point, but I would have had to make picoAccordion bigger so that it would have another trait. Then, I would have had to get rid of the *Small* bullet point which means I really would have needed to add *two* traits. I'll just leave this here in the event it develops further in the future.
+
+## Usage
+It's really easy. Watch this.
+
+    <dl class="accordion">
+        <dt>Heading 1</dt>
+        <dd>The first thing I want to say.</dd>
+        <dt>Heading 2</dt>
+        <dd>Another thing to say.</dd>
+        <dt>Heading 3</dt>
+        <dd>Still another thing for saying.</dd>
+    </dl>
+
+This is a definition list, so I have three different pairs. In this example, my terms (`dt` elements) will be clickable to expand the definitions (`dd` elements). Since picoAccordion already knows how the definition list should be structured, all you have to do is run the init method on the list itself. Here's what that would look like if you wanted to do it inline on the page:
+
+    <script>
+        $('.accordion').picoAccordion();
+    </script>
+
+Simple, right? Put that script at the bottom of the page just inside the closing `body` tag. This assumes you have already imported jQuery before you run this script. If not, it won't work.
+
+## Thanks!
+That's about it. If you like and/or use this plugin, I'd love to hear from you [on Twitter](http://twitter.com/raddevon).
